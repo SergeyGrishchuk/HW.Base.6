04d5d0d3eef1b7ac64dbbf2a5c03b588ef0e779d
SergeyGrishchuk <sirgeyaid666@gmail.com>
2020-08-08 15:04:23 +0300

    HomeWork6

--- src/HomeWork6/Main.java
+++ src/HomeWork6/Main.java
@@ -1,8 +1,17 @@
 package HomeWork6;
 
+import HomeWork6.animals.Cat;
+import HomeWork6.animals.Dog;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Cat cat = new Cat(150,10);
+        Dog dog = new Dog(350, 9);
+
+        cat.swimming();
+        dog.running();
+
     }
 }
+
---
+++ src/HomeWork6/animals/Dog.java
@@ -0,0 +1,8 @@
+package HomeWork6.animals;
+
+public class Dog extends Animal{
+
+    public Dog(int run, int swim) {
+        super(run, swim);
+    }
+}
